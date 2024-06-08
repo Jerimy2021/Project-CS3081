0d215ceeee2650cf65b3004248bee20a88869801
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2024-06-08 13:26:33 -0500

    get_textures añadido y puntos x,y,z

--- backend/api.py
+++
@@ -1,7 +0,0 @@
-import implements_api
-
-def GetPlanets(SystemSolar):
-    return implements_api.fetch_name_planets(SystemSolar)
-
-def GetStellarSystems():
-    return implements_api.fetch_stellar_hosts()
